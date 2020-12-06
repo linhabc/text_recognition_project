8dc037acd6c99181a81c28869487a2f142da4cc3
linh nguyen <jonathanrocrach2@gmail.com>
2020-12-06 23:08:46 +0700

    update

---
+++ lib/display_screen.dart
@@ -0,0 +1,80 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http_parser/http_parser.dart';
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Display the Picture')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          uploadFile(context, imagePath);
+        },
+        child: Icon(Icons.upload_file),
+      ),
+      body: Image.file(File(imagePath)),
+    );
+  }
+}
+
+uploadFile(BuildContext context, var imagePath) async {
+  var postUri = Uri.parse("http://157.230.32.117:5001/predict");
+
+  var request = new http.MultipartRequest("POST", postUri);
+  request.files.add(
+    new http.MultipartFile(
+      'file',
+      File(imagePath).readAsBytes().asStream(),
+      File(imagePath).lengthSync(),
+      filename: imagePath,
+      contentType: new MediaType('image', 'png'),
+    ),
+  );
+
+  var response = await request.send();
+  print(response.statusCode);
+
+  // listen for response
+  if (response.statusCode == 200)
+    response.stream.transform(utf8.decoder).listen((Object value) {
+      showAlertDialog(context, value);
+      print(value);
+    });
+  else
+    showAlertDialog(context, "Cant get value");
+}
+
+showAlertDialog(BuildContext context, String text) {
+  // set up the button
+  Widget okButton = FlatButton(
+    child: Text("OK"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
+
+  AlertDialog alert = AlertDialog(
+    title: Text("The text is"),
+    content: Text(text),
+    actions: [
+      okButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart';
+import 'package:text_recognition_project/display_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -95,20 +96,6 @@ class TakePictureScreenState extends State<TakePictureScreen> {
           }
         },
       ),
-    );
-  }
-}
-
-class DisplayPictureScreen extends StatelessWidget {
-  final String imagePath;
-
-  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Display the Picture')),
-      body: Image.file(File(imagePath)),
     );
   }
 }
